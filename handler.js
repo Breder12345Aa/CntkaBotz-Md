03340d6215009585c2161639eeac1bc86a541e63
Breder12345Aa <82520506+Breder12345Aa@users.noreply.github.com>
2022-07-24 01:09:41 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -894,15 +894,15 @@ To turn off this feature, type
 
 global.dfail = (type, m, conn) => {
     let msg = {
-        rowner: '*Lu siapa?, Cuma Ayang Fachri Yang Bisa*',
-        owner: '*Lu siapa?, Cuma Ayang Fachri Yang Bisa*!',
-        mods: '*Lu siapa?, Cuma Ayang Fachri Yang Bisa*!',
+        rowner: '*Lu siapa?, Cuma Owner Yang Bisa*',
+        owner: '*Lu siapa?, Cuma Creator Bot Yang Bisa*!',
+        mods: '*Lu siapa?, Cuma Mods Bot Yang Bisa*!',
         premium: '*Beli premium dulu lah, murah kok!*',
         group: '*Bisa nya di Group kak!*',
         private: '*Bisanya di Private Chat kak!*',
         admin: '*Lu admin kah?, kalo bukan gk bisa cuy:v!*',
         botAdmin: '*Gw bukan admin "-_ Jadiin admin dulu lah!*',
-        unreg: 'Silahkan daftar untuk menggunakan fitur ini dengan cara mengetik:\n\n*#daftar nama.umur*\n\nContoh: *#daftar Fachri.16*',
+        unreg: 'Silahkan daftar untuk menggunakan fitur ini dengan cara mengetik:\n\n*#daftar nama.umur*\n\nContoh: *#daftar BYE.16*',
         restrict: 'This feature is *disabled*!'
     }[type]
     if (msg) return conn.reply(m.chat, msg, m, { contextInfo: { externalAdReply: {title: global.wm, body: '404 Access denied!', sourceUrl: sgc, thumbnail: fs.readFileSync('./thumbnail.jpg') }}})
